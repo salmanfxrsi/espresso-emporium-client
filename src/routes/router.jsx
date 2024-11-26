eb545856644f8519e1d9f304dd79d27446b06e2e
salmanfxrsi <salmanxprivate@gmail.com>
2024-11-27 01:22:10 +0600

    feat: initialized Espresso Emporium client with React Router, Tailwind, favicon, and meta setup

---
+++ src/routes/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Espresso Emporium</div>,
+  },
+]);
+
+export default router;
