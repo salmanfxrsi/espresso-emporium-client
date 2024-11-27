cb6d97eaa8c54442d7a4ea1f27f85ef3022e8b22
salmanfxrsi <salmanxprivate@gmail.com>
2024-11-27 12:55:34 +0600

    feat: add FollowUs section with Instagram integration and coffee-themed photos

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,12 +1,23 @@
+import { Outlet } from "react-router-dom";
 import Header from "../components/HomePage/Header";
 import Highlights from "../components/HomePage/Highlights";
+import InstaTrail from "../components/HomePage/InstaTrail";
 
 const HomeLayout = () => {
-    return (
-        <div>
-            <header><Header></Header><Highlights></Highlights></header>
+  return (
+    <div>
+      <header>
+        <Header></Header>
+        <Highlights></Highlights>
+      </header>
+      <main className="py-[120px] bg-home-outlet">
+        {/* InstaTrail Section */}
+        <div className="container mx-auto">
+          <InstaTrail></InstaTrail>
         </div>
-    );
+      </main>
+    </div>
+  );
 };
 
 export default HomeLayout;
